d5988f3e841ddf6e34a13121f0edb7244a1ef53e
Aizhan-15 <aizhanurman@gmail.com>
2022-09-26 18:52:41 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -3,11 +3,12 @@ package school.mjc.stage0.conditions.task3;
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
         char symbol1 = 'a';
-        for (int i = 0; i < symbol1.length(); i++)
-            if (symbol1.charAt(i) > 'A' && symbol1.charAt(i) < 'Z') {
-                System.out.println("English");
-            } else {
-                System.out.println("Non English");
-            }
+        if (symbol1 == 'a' || symbol1 == 'b' || symbol1 == 'c' || symbol1 == 'd' || symbol1 == 'e' || symbol1 == 'f' || symbol1 == 'g' || symbol1 == 'h' || symbol1 == 'i' || symbol1 == 'j' || symbol1 == 'k' || symbol1 == 'l' || symbol1 == 'm' || symbol1 == 'n' || symbol1 == 'o' || symbol1 == 'p' || symbol1 == 'q' || symbol1 = 'r' || symbol1 == 's' || symbol1 == 't' || symbol1 == 'u' || symbol1 == 'v' || symbol1 == 'w' || symbol1 == 'x' || symbol1 == 'y' || symbol1 == 'z') {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
+
     }
 }
+
