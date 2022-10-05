0e5e1388cbdeebbc96f2c51b4824f412073ce35e
Aizhan-15 <aizhanurman@gmail.com>
2022-10-05 07:59:23 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -6,12 +6,11 @@ public class IsEnglishSymbolDeterminer {
         if (symbol1 == 'a' || symbol1 == 'b' || symbol1 == 'c' || symbol1 == 'd' || symbol1 == 'e' || symbol1 == 'f' || symbol1 == 'g' || symbol1 == 'h' || symbol1 == 'i') {
             System.out.println("English");
         } else {
-            System.out.println("Non English");
-        }
-        if (symbol1 == 'A' || symbol1 == 'B' || symbol1 == 'C' || symbol1 == 'D' || symbol1 == 'E' || symbol1 == 'F' || symbol1 == 'G' || symbol1 == 'H' || symbol1 == 'I') {
-            System.out.println("English");
-        } else {
-            System.out.println("Non English");
+            if (symbol1 == 'A' || symbol1 == 'B' || symbol1 == 'C' || symbol1 == 'D' || symbol1 == 'E' || symbol1 == 'F' || symbol1 == 'G' || symbol1 == 'H' || symbol1 == 'I') {
+                System.out.println("English");
+            } else {
+                System.out.println("Non English");
+            }
         }
     }
 }
