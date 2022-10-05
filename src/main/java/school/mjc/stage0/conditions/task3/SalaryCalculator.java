88e562a8b316094ff533f4c60a334d2a41151eaa
Aizhan-15 <aizhanurman@gmail.com>
2022-10-05 07:43:52 +0600

    completed all the tasks

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        int salary1 = 5000;
+        int salary1 = 25000;
         if (salary1 <= 10000) {
             System.out.println(salary1 * 0.15);
         } else {
